15130b7df6ab3dcbcd0d04b3182b5ee5af13bc29
mysoyul <vega2k_@naver.com>
2025-01-22 10:47:57 +0900

    test Optional orElseGet add

--- src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
+++ src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,7 +17,7 @@ class CustomerRepositoryTest {
 
     @Test
     void insert_select() {
-//Entity 객체생성
+        //Entity 객체생성
         Customer customer = new Customer();
         customer.setCustomerId("A001");
         customer.setCustomerName("스프링");
@@ -26,5 +28,20 @@ class CustomerRepositoryTest {
         assertThat(savedCustomer).isNotNull();
         assertThat(savedCustomer.getCustomerName()).isEqualTo("스프링");
         assertEquals("A001",savedCustomer.getCustomerId());
+
+        //PK로 조회하기
+        Optional<Customer> byId = repository.findById(1L);
+        if(byId.isPresent()){
+            Customer existCustomer = byId.get();
+            assertThat(existCustomer.getCustomerName()).isEqualTo("스프링");
+        }
+        //Optional orElseGet
+        Customer notFoundCust =
+                repository.findByCustomerId("B001").orElseGet(() -> new Customer());
+        assertThat(notFoundCust.getCustomerId()).isNullOrEmpty();
+
+        //Optional orElseThrow
+
+
     }
 }
